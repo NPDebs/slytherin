f11c00d94603a227ccfa13e7f2a59bb437621b50
NPDebs <deborahudoh02@gmail.com>
2024-01-10 17:36:24 +0100

    Explore "more comprehensive" library
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- spell_checker.py
+++ spell_checker.py
@@ -1,21 +1,24 @@
-# Import textblob library.
-from textblob import TextBlob
+import enchant
 
-# Initialise a loop to allow the user input words until they choose to exit.
+def spell_check(text):
+    checker = enchant.Dict("en_US")
+    words = text.split()
+    corrected_words = [checker.suggest(word)[0] if not checker.check(word) else word for word in words]
+    return ' '.join(corrected_words)
+
 t = 1
 while t:
-    # accept user input and print word
     word_input = input("Enter the text to be checked: ")
-    print("Your original text: "+str(word_input))
-    
-    # Create an object from the user's input.  
-    blob = TextBlob(word_input)  
+    print("Your original text: " + str(word_input))
 
-    if word_input.lower() == str(blob.correct()).lower():
+    corrected_text = spell_check(word_input)
+    
+    if word_input.lower() == corrected_text.lower():
         print("The text is already correct.")
     else:
-        # Use correct() method to correct the spelling of the text; print the corrected spelling
-        corrected_text = blob.correct()
-        print("corrected text: "+str(corrected_text))
+        print("Corrected text: " + corrected_text)
     
     t = int(input("Do you want to correct another text? 1 (yes) : 0 (no) "))
+
+
+# TO-DO: Figure out why the Dict attribute is not recognised here, when the docs beg to differ
