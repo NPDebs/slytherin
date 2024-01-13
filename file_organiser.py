4f627b36b27b94ac5416f9d450a6feb61c9435b9
NPDebs <deborahudoh02@gmail.com>
2024-01-13 20:04:34 +0100

    Define and test first part of "organise_files" function
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- file_organiser.py
+++ file_organiser.py
@@ -10,5 +10,9 @@ def organise_files(starting_folder, destination_folder):
 
     # Make a list of files in the starting_folder and store in "files"
     files = os.listdir(starting_folder)
+    print(files)
 
     # Loop through the list of files and extract 
+    for file in files:
+        starting_path = os.path.join(starting_folder, file)
+        print(starting_path)
