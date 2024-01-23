739560c0ecee57c4c743303dc74d008f77c7b422
NPDebs <deborahudoh02@gmail.com>
2024-01-23 21:12:49 +0100

    Implement conversion using the 'to_markdown' method and write result into new file
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- pandas.py
+++ pandas.py
@@ -9,3 +9,14 @@ csv_file => path to the CSV file & md_output => path to the Markdown file
 def csv_to_markdown(csv_file, md_output):
     # Read the CSV file into a Pandas DataFrame, df
     df = pd.read_csv(csv_file)
+
+    '''
+    Use the "to_markdown" method of the DataFrame (df) to convert it into a Markdown table.
+    The "index=False" parameter specifies that the DataFrame index should not be included in the Markdown table.
+    '''
+    
+    markdown_table = df.to_markdown(index=False)
+
+    # Open the specified md file (md_output) in write mode; write the md table content into it
+    with open(md_output, 'w') as md_file:
+        md_file.write(markdown_table)
