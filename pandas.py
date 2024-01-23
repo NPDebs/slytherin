db6ffe92ca6667b2bcb9a585709ab807096b04af
NPDebs <deborahudoh02@gmail.com>
2024-01-23 21:08:43 +0100

    Define function to handle conversion
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- pandas.py
+++ pandas.py
@@ -1,2 +1,11 @@
 # import panda library => for data manipulation/analysis
 import pandas as pd
+
+'''
+Define a function that accepts two parameters: csv_file & md_output
+csv_file => path to the CSV file & md_output => path to the Markdown file
+'''
+
+def csv_to_markdown(csv_file, md_output):
+    # Read the CSV file into a Pandas DataFrame, df
+    df = pd.read_csv(csv_file)
