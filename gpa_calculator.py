5981e45fab79f46c3c2539115773d6606ddc80e2
NPDebs <deborahudoh02@gmail.com>
2024-01-27 20:18:10 +0100

    Comment containing the operational definition of terms
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

---
+++ gpa_calculator.py
@@ -0,0 +1,9 @@
+'''
+GRADE => the letter grade corresponding to one's numerical score (e.g 72 = A).
+GP (Grade Point) => the standardised numerical value assigned to the letter grade (e.g., A=5.0, B=4.0).
+QP (Quality Points): Represents the product of the Grade Points and the number of credit units for the course.
+    QP = Unit * GP
+
+GPA => Grade Point Average: A numeric representation of academic performance.
+This is the sum of quality points divided by the total number of credits.
+'''
