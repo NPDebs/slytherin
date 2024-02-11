9c8189a1c61eb3e701217aac54430ec4f30a333e
NPDebs <deborahudoh02@gmail.com>
2024-02-11 11:42:29 +0100

    Start new app "gpa_calculator"
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

---
+++ slytherin/gpa_calculator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GpaCalculatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gpa_calculator'
---
+++ slytherin/gpa_calculator/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def gpa_calculator(request):
+    return HttpResponse("Hello world!")
