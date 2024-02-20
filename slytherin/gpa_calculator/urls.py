e06f917a2dd903bbfcbcfd8482a74abef69054a3
NPDebs <deborahudoh02@gmail.com>
2024-02-20 19:28:28 +0100

    Map new urls to views
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- slytherin/gpa_calculator/urls.py
+++ slytherin/gpa_calculator/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gpa_calculator/', views.gpa_calculator, name='gpa_calculator'),
+    # Map the URL 'gpa_calculator/' to the 'gpa_form' view
+    path('gpa_calculator/', views.gpa_form, name='gpa_form'),
+    
+    # Map the URL 'gpa_calculator/calculate' to the 'gpa_calculator' view
+    path('gpa_calculator/calculate/', views.gpa_calculator, name='gpa_calculator'),
 ]
